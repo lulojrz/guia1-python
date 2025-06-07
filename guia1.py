1b85a16cfe5252f70c17cb314f6d301ce5e1521a
lulojrz <109257773+lulojrz@users.noreply.github.com>
2025-06-07 19:58:10 -0300

    Update guia1.py

--- guia1.py
+++ guia1.py
@@ -1,69 +1,70 @@
 
 #ejercicio 1
 import math
-def imprimir_holaMundo() ->str:
+def imprimir_holaMundo():
    print("hola mundo")
 
-def imprimir_verso()-> str:
+def imprimir_verso():
   print("Estoy mirando atras\n y puedo ver mi vida entera \n y se que estoy en paz \n pues la vivi a mi manera")
 
 
-def raiz_de2()->str:
-    print("la raiz cuadrada de 2 es: " ,  round(math.sqrt(2),4))
+def raiz_de2():
+   return round(math.sqrt(2),4)
 
 
-def factorial_de_2()->str:
-  print("factorial de 2 es " , math.factorial(2))
+def factorial_de_2() -> float:
+  return  math.factorial(2)
 
-def perimetro()->str:
-  print("el perimetro de una circunferencia de radio 1 es : " , math.pi * 2)
+def perimetro()-> float:
+  return  math.pi * 2
 
 #ejercicio 2
 
-def imprimir_saludo(nombre)->str:
+def imprimir_saludo(nombre:str):
   print("hola" , nombre)
   
 
-def raiz_cuadrada(num:int)->str:
+def raiz_cuadrada(num:int)->float:
   return  math.sqrt(num)
 
-def  fahrenheit_a_celsius (temp:int)->str :
-  print("la temperatura convertida de fahrenheit a celsius es :" ,
-       round((((temp-32)*5)/9),1)  )
+def  fahrenheit_a_celsius (temp:float)->float :
+  
+  return  round((((temp-32)*5)/9),1)  
 
-def imprimir_dos_veces(estribillo:str)->str:
+def imprimir_dos_veces(estribillo:str):
   print(estribillo*2)
 
 
-def es_multiplo_de(x:int,y:int)->str:
-  if (x % y ==0):
-   print("son multiplos")
-  else:
-    print("no son multiplos")
+def es_multiplo_de(x:int,y:int)->bool:
+    res = False
+    if (y % x ==0):
+     res = True
+
+    return res
 
 
-def es_par(x:int)->str:
-  es_multiplo_de(x,2)
+def es_par(x:int)->bool:
+   return es_multiplo_de(2,x)
 
 
 def cantidad_de_pizzas(comensales:int,minimoxpersona:int)->int :
    total_porciones = comensales*minimoxpersona
-   print("se necesitan ", math.ceil(total_porciones / 8 ))
+   return math.ceil(total_porciones / 8 )
 
 #ejercicio 3
 def alguno_es_0(num1:int,num2:int)-> bool:
-  print(num1==0  or num2==0)
+   return num1==0  or num2==0
 
 def ambos_son_0(num1:int,num2:int)->bool:
-  print(num1==0 and num2 ==0)
+  return num1==0 and num2 ==0
 
 
 def es_nombre_largo(nombre:str)->bool:
-  print(len(nombre) > 3 and len(nombre) < 8 )
+  return len(nombre) > 3 and len(nombre) < 8
 
 
 def año_bisiesto(año:int)->bool:
-  print((año%400 == 0 ) or (año % 100 != 0 and año%4==0)  )
+  return (año%400 == 0 ) or (año % 100 != 0 and año%4==0)
 
 #ejercicio 4
 def peso_pino(arbol:int)-> int:
@@ -71,8 +72,8 @@ def peso_pino(arbol:int)-> int:
     return arbol*3
   else:
     
-    calc = arbol - 300
-    return (calc*2 )+ 900
+   calc = arbol - 300
+  return (calc*2 )+ 900
 
 
 def es_peso_util(peso:int)->bool: 
@@ -96,17 +97,27 @@ def devolver_valor_si_es_par_sino_el_que_sigue(num:int)->int:
     return num
   else:
     return num+1
+  
+def devolver_doble_simultiplode3_devolvertriple_simultiplode9(num:int)->int:
+  if (num%3==0):
+    return num*2
+  elif (num%9==0):
+    return num*3
+  else:
+    return num 
+  
+  
 
 
 
-def lindo_nombre(nombre:str)->str:
+def lindo_nombre(nombre:str):
    if(len(nombre)<5):
      print("tu nombre es corto")
    else:
      print("tu nombre es largo")
 
 
-def elRango(num:int)->str:
+def elRango(num:int):
   if (num<5):
     print("menor a 5")
   elif (num > 10 and num<20):
@@ -115,7 +126,7 @@ def elRango(num:int)->str:
     print("mayor a 20")
 
 
-def vacaciones_o_trabajo(sexo:str,edad:int)-> str:
+def vacaciones_o_trabajo(sexo:str,edad:int):
   if (sexo=="Masculino" or sexo =="masculino"):
        if (edad<=18 or edad >=65):
          print("andate de vacaciones")
@@ -129,68 +140,77 @@ def vacaciones_o_trabajo(sexo:str,edad:int)-> str:
 
 #ejercicio 6
 
-#i = 1
-#while(i<=10):
-#  print(i)
-#  i+=1
+def imprimir_num_1a10():
+  i = 1
+  while(i<=10):
+   print(i)
+   i+=1
 
 
-#x = 10
-#while(x<=40):
- # if(x%2==0):
-  #  print(x)
-   # x+=1
- # else:
-  #  x+=1
+def imprimir_pares_10a40():
+ x = 10
+ while(x<=40):
+  if(x%2==0):
+    print(x)
+    x+=1
+  else:
+    x+=1
 
+def imprimir_eco_10veces():
+  
+ i = 1
+ while(i<=10):
+  print("eco")
+  i+=1
 
-#i = 1
-#while(i<=10):
- #print("eco")
- #i+=1
-
-def cuentaregresiva(num:int)-> str:
+def cuentaregresiva(num:int):
   while (num >=1):
     print(num)
     num-=1
 
 
-def viaje_en_el_tiempo(añoActual:int,añoPasado:int)-> str:
+def viaje_en_el_tiempo(añoActual:int,añoPasado:int):
   while añoActual>= añoPasado:
     print("esta en el año", añoActual)
     añoActual-=1
 
 
-def viaje_en_el_tiempo_2(añoActual:int)-> str:
+def viaje_en_el_tiempo_2(añoActual:int):
   año = -384
   while añoActual>= año:
     print("esta en el año", añoActual)
     añoActual-=20
 
 #ejercicio 7
-#for num in range(1,11,1):
-# print(num)
 
-#for num in range(10,41,1):
- #if(num%2==0):
-  #print(num)
+def imprimir_num_1a10_for(num:int):
+ for num in range(1,11,1):
+  print(num)
 
-#for num in range(1,11,1):
- # print("eco")
-
-#def despegue_2(num):
- #  for i in range(num,-1,-1):
-  #    print(i)
+def imprimir_pares_10a40_for(num:int):
+  
+ for num in range(10,41,1):
+  if(num%2==0):
+   print(num)
 
 
-#def viaje_en_el_tiempo2_for(añoActual:int,AñoDeseado:int)-> str:
- #  for num in range(añoActual,AñoDeseado-1,-1):
-  #    print("estamos en el año" , num)
+def imprimir_eco_10veces_for(num:int):
+ for num in range(1,11,1):
+   print("eco")
+
+def despegue_2(num:int):
+   for i in range(num,-1,-1):
+      print(i)
+
+
+def viaje_en_el_tiempo2_for(añoActual:int,AñoDeseado:int)-> str:
+   for num in range(añoActual,AñoDeseado-1,-1):
+      print("estamos en el año" , num)
   
 
-#def viaje_a_aristoteles(añoActual:int)-> str:
- #  for num in range(añoActual,383,-20):
-  #    print("estamos en el año", num)
+def viaje_a_aristoteles(añoActual:int)-> str:
+   for num in range(añoActual,383,-20):
+      print("estamos en el año", num)
 
 #ejercicio 8
 #son pavadas
@@ -200,7 +220,7 @@ def rt(x: int, g: int)-> int:
    g = g + 1
    return x + g
   
-g: int = 0
+
 def ro(x: int)-> int:
   global g
   g = g + 1
@@ -230,5 +250,3 @@ def evaluar_3_veces()-> int:
 
 
 resultado_2 = evaluar_3_veces()  
-
-
